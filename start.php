301f76dcb3a3d59b79e51413e35004313d7c1797
Sem <sembrestels@riseup.net>
2012-08-20 21:45:36 +0200

    Made page icons smaller.

--- start.php
+++ start.php
@@ -12,6 +12,11 @@ function n1_theme_init() {
 	
 	elgg_register_simplecache_view('n1_theme/landing');
 	elgg_register_css('n1:landing_page', elgg_get_simplecache_url('css', 'n1_theme/landing'));
+	
+	if (elgg_is_active_plugin('pages')) {
+		// pages icon url override
+		elgg_register_plugin_hook_handler('entity:icon:url', 'object', 'n1_theme_pages_icon_url_override');
+	}
 }
 
 function n1_theme_topbar_menu($hook, $type, $returnvalue, $params){
@@ -21,4 +26,28 @@ function n1_theme_topbar_menu($hook, $type, $returnvalue, $params){
 function n1_theme_front_page($hook, $type, $returnvalue, $params) {
 	include(elgg_get_plugins_path() . 'n1_theme/index.php');
 	return true;
+}
+
+/**
+ * Override the default entity icon for pages
+ *
+ * @return string Relative URL
+ */
+function n1_theme_pages_icon_url_override($hook, $type, $returnvalue, $params) {
+	$entity = $params['entity'];
+	if (elgg_instanceof($entity, 'object', 'page_top') ||
+		elgg_instanceof($entity, 'object', 'page')) {
+		switch ($params['size']) {
+			case 'topbar':
+			case 'tiny':
+				return 'mod/n1_theme/_graphics/pages_icons/pages_tiny.gif';
+				break;
+			case 'small':
+				return 'mod/n1_theme/_graphics/pages_icons/pages_small.gif';
+				break;
+			default:
+				return 'mod/pages/images/pages_lrg.gif';
+				break;
+		}
+	}
 }
