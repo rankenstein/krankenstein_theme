5284160a2165be944653b11fbba2d131dbe9a716
foockinho <foockinho@lorea.org>
2012-10-09 01:33:34 +0200

    fixed elgg-search-input[text] height, fixed elgg-sprites.png css

--- views/default/css/elements/icons.php
+++ views/default/css/elements/icons.php
@@ -34,7 +34,7 @@
 	background-position: 0 -72px;
 }
 .elgg-icon-calendar {
-	background-position: 0 -90px;
+	background-position: 0 -91px;
 }
 .elgg-icon-cell-phone {
 	background-position: 0 -108px;
@@ -70,7 +70,7 @@
 	background-position: 0 -288px;
 }
 .elgg-icon-download {
-	background-position: 0 -306px;
+	background-position: 0 -304px;
 }
 .elgg-icon-eye {
 	background-position: 0 -324px;
@@ -148,10 +148,10 @@
 	background-position: 0 -756px;
 }
 .elgg-icon-refresh:hover {
-	background-position: 0 -774px;
+	background-position: 0 -775px;
 }
 .elgg-icon-refresh {
-	background-position: 0 -792px;
+	background-position: 0 -791px;
 }
 .elgg-icon-round-arrow-left {
 	background-position: 0 -810px;
@@ -175,7 +175,7 @@
 	background-position: 0 -918px;
 }
 .elgg-icon-search {
-	background-position: 0 -936px;
+	background-position: 0 -934px;
 }
 .elgg-icon-settings-alt:hover {
 	background-position: 0 -954px;
@@ -270,7 +270,7 @@
 	background-position: 0 -1476px;
 }
 .elgg-icon-users {
-	background-position: 0 -1494px;
+	background-position: 0 -1493px;
 }
 .elgg-icon-video {
 	background-position: 0 -1512px;
